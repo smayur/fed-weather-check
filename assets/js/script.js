e1122d5eb9f0f25c9744e4131782be1883c27d31
mayurS <mayurs@prdxn.com>
2020-11-29 20:53:34 +0530

    navbar toggling script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,11 @@
-/* Author: 
+(function() {
 
-*/
+  var humberger = document.querySelector('.burger');
+  var navbar = document.querySelector('.navbar');
 
+  // Function to hide and show navbar on click of humberger icon.
+  humberger.addEventListener('click', function() {
+    navbar.classList.toggle('hide-navbar');
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})();
